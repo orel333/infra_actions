daed561302de8bae698388238bdcfd93f057266c
orel.aleksey <orel.aleksey@wb.ru>
2022-12-05 00:23:43 +0300

    Последний урок курса

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('Крутое получилось плаванье!')
 
 
 def second_page(request):
