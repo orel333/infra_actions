3e5f757f1bdaa4f874a70c3f7f7a4cd63c509de2
orel.aleksey <orel.aleksey@wb.ru>
2022-12-12 14:55:51 +0300

    Добавил порт в allowed hosts

--- infra_project/infra_project/settings.py
+++ infra_project/infra_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('DJANGO_KEY', default='some_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('MYHOST', default='127.0.0.1').split(',')
+ALLOWED_HOSTS = os.getenv('MYHOST').split(',')
 
 
 # Application definition
